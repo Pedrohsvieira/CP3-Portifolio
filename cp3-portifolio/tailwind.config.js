5aeb287b2811455fd16f9f219ccbf5b940395ad9
Pedrohsvieira <phhenriquevieira20@gmail.com>
2024-10-30 16:52:55 -0300

    Configurando a estilização do projeto

--- cp3-portifolio/tailwind.config.js
+++ cp3-portifolio/tailwind.config.js
@@ -1,10 +1,20 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1E40AF",
+        secondary: "#4F46E5",
+        accent: "#818CF8",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
